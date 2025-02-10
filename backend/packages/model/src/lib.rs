cfba6691923da841ea227df5716b6297249c4793
BVT - Abdul Haris Djafar <bvt@BVTs-MacBook-Air.local>
2025-02-10 17:37:41 +0700

    update code auth integration

--- backend/packages/model/src/lib.rs
+++ backend/packages/model/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod authorization;
 pub mod domain;
 pub mod surreal_db;
 pub mod web;
--- backend/packages/state/src/axum.rs
+++ backend/packages/state/src/axum.rs
@@ -1,5 +1,8 @@
+use redis::Client;
 use service::user::user_service::UserService;
+
 #[derive(Clone)]
 pub struct AppState {
     pub user_service: UserService,
+    pub redis_client: Client,
 }
