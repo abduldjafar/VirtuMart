8c326d2078562872849f3c88887cb3a9c4f7ad06
Abdul Haris Djafar <abdulharisdjafar@Abduls-MacBook-Pro.local>
2025-03-06 09:23:03 +0700

    update code simplyfy api test

--- backend/tests/api_test.rs
+++ backend/tests/api_test.rs
@@ -8,7 +8,7 @@ mod tests {
             #[tokio::test]
             async fn $name() -> Result<()> {
                 let hc = httpc_test::new_client("http://localhost:3000")?;
-                
+
                 let response = match stringify!($method) {
                     "GET" => hc.do_get($endpoint).await?,
                     "POST" => hc.do_post($endpoint, $payload).await?,
@@ -24,14 +24,24 @@ mod tests {
 
     api_test!(test_health, GET, "/api/health", json!({}));
     api_test!(test_swagger_run, GET, "/swagger-ui/", json!({}));
-    api_test!(test_login, POST, "/api/v1/login", json!({
-        "email": "test@example.com",
-        "password": "password123"
-    }));
-    api_test!(test_register, POST, "/api/v1/user", json!({
-        "email": "abdule@example.com",
-        "password": "1234567890",
-        "role": "admin",
-        "username": "asoi909090"
-    }));
+    api_test!(
+        test_login,
+        POST,
+        "/api/v1/login",
+        json!({
+            "email": "test@example.com",
+            "password": "password123"
+        })
+    );
+    api_test!(
+        test_register,
+        POST,
+        "/api/v1/user",
+        json!({
+            "email": "abdule@example.com",
+            "password": "1234567890",
+            "role": "admin",
+            "username": "asoi909090"
+        })
+    );
 }
