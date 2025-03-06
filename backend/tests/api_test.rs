255634a6cee04838c9552bf5469f7454a4527fac
Abdul Haris Djafar <abdulharisdjafar@Abduls-MacBook-Pro.local>
2025-03-06 09:22:36 +0700

    update code simplyfy api test

--- backend/tests/api_test.rs
+++ backend/tests/api_test.rs
@@ -3,55 +3,35 @@ mod tests {
     use anyhow::{Ok, Result};
     use serde_json::json;
 
-    #[tokio::test]
-    async fn test_health() -> Result<()> {
-        let hc = httpc_test::new_client("http://localhost:3000")?;
-        let response = hc.do_get("/api/health").await?;
-        assert_eq!(response.status(), 200);
-        Ok(())
+    macro_rules! api_test {
+        ($name:ident, $method:ident, $endpoint:expr, $payload:expr) => {
+            #[tokio::test]
+            async fn $name() -> Result<()> {
+                let hc = httpc_test::new_client("http://localhost:3000")?;
+                
+                let response = match stringify!($method) {
+                    "GET" => hc.do_get($endpoint).await?,
+                    "POST" => hc.do_post($endpoint, $payload).await?,
+                    _ => panic!("Unsupported HTTP method"),
+                };
+
+                assert_eq!(response.status(), 200);
+
+                Ok(())
+            }
+        };
     }
 
-    #[tokio::test]
-    async fn test_swagger_run() -> Result<()> {
-        let hc = httpc_test::new_client("http://localhost:3000")?;
-        let response = hc.do_get("/swagger-ui/").await?;
-        assert_eq!(response.status(), 200);
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_login() -> Result<()> {
-        let hc = httpc_test::new_client("http://localhost:3000")?;
-
-        let payload = json!({
-            "email": "test@example.com",
-            "password": "password123"
-        });
-
-        let response = hc.do_post("/api/v1/login", payload).await?;
-
-        println!("{:?}", response.json_body()?);
-        assert_eq!(response.status(), 200);
-
-        Ok(())
-    }
-
-    #[tokio::test]
-    async fn test_register() -> Result<()> {
-        let hc = httpc_test::new_client("http://localhost:3000")?;
-
-        let payload = json!({
-          "email": "abdule@example.com",
-          "password": "1234567890",
-          "role": "admin",
-          "username": "asoi909090"
-        });
-
-        let response = hc.do_post("/api/v1/user", payload).await?;
-
-        println!("{:?}", response.json_body()?);
-        assert_eq!(response.status(), 200);
-
-        Ok(())
-    }
+    api_test!(test_health, GET, "/api/health", json!({}));
+    api_test!(test_swagger_run, GET, "/swagger-ui/", json!({}));
+    api_test!(test_login, POST, "/api/v1/login", json!({
+        "email": "test@example.com",
+        "password": "password123"
+    }));
+    api_test!(test_register, POST, "/api/v1/user", json!({
+        "email": "abdule@example.com",
+        "password": "1234567890",
+        "role": "admin",
+        "username": "asoi909090"
+    }));
 }
